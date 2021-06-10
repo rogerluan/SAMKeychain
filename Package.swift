5670ba17a3176fee626612bcd7644db3b405d4fa
Roger Oba <rogerluan.oba@gmail.com>
2021-06-09 22:11:45 -0300

    Move from CocoaPods to SPM.

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SAMKeychain",
+    platforms: [ .iOS(.v13) ],
+    products: [
+        .library(name: "SAMKeychain", targets: ["SAMKeychain"]),
+    ],
+    targets: [
+        .target(
+            name: "SAMKeychain",
+            path: "Sources",
+            publicHeadersPath: "."
+        ),
+        .testTarget(
+            name: "SAMKeychainTests",
+            dependencies: ["SAMKeychain"],
+            path: "Tests"
+        ),
+    ]
+)
